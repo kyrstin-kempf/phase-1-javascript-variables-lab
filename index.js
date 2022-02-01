cc3e78491eb5617cc2abc59e175d6ba5527ceb96
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2022-01-31 20:49:01 -0500

    commit tree 975147c0b6d99bd2ff87779649e9372c05cacb87

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+// let companyName = 'Scuber';
+
+/* let assignment can be changed, declared name cannot
+const cannot change assignment nor name */
+
+const companyName = 'Scuber';
+
+let mostProfitableNeighborhood = 'Chelsea';
+
+let companyCeo = 'Susan Smith';
